d5201f97da802e7ef86f9c872e863cc2e55a371a
karkill9 <kartikmm99@gmail.com>
2023-11-09 21:41:05 +0530

    Added Redux-Store Structure

--- Connect_Work_Hub/componennts/UserDetails/userDetails.js
+++ Connect_Work_Hub/componennts/UserDetails/userDetails.js
@@ -1,79 +1,89 @@
-
-import { FormControl, Input, Stack, WarningOutlineIcon, Box, NativeBaseProvider, Select, Checkbox, Button } from "native-base";
+import {
+  FormControl,
+  Input,
+  Stack,
+  WarningOutlineIcon,
+  Box,
+  NativeBaseProvider,
+  Select,
+  Checkbox,
+  Button,
+} from "native-base";
 import { Text } from "react-native";
 // import { useEffect } from "react";
 
-
-export default function UserDetails({navigation}) {
-
-    const handleFirstForm = async () => {
-        try {
-          const response = await axios.post('https://connect-hub-backend.onrender.com/auth/signup', {
-          username:userName,
-          password:password,
-          usertype:userType,
-          name:name,
-          mobile:mobile,
-          alternatemobile:alternateMobile,
-          address:address,
-  state:state,
-  city:city,
-  pinCode:pinCode,
-  gender:gender
-  
-          });
-          console.log('Response:', response.data);
-          // Handle successful response
-        } catch (error) {
-          console.log(error)
+export default function UserDetails({ navigation }) {
+  const handleFirstForm = async () => {
+    try {
+      const response = await axios.post(
+        "https://connect-hub-backend.onrender.com/auth/signup",
+        {
+          username: userName,
+          password: password,
+          usertype: userType,
+          name: name,
+          mobile: mobile,
+          alternatemobile: alternateMobile,
+          address: address,
+          state: state,
+          city: city,
+          pinCode: pinCode,
+          gender: gender,
         }
-      }  
+      );
+      console.log("Response:", response.data);
+      // Handle successful response
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  return (
+    <NativeBaseProvider>
+      <Box alignItems="center">
+        <Box w="100%" maxWidth="300px">
+          <FormControl isRequired>
+            <Stack mx="4">
+              <FormControl.Label>Submit Your details</FormControl.Label>
+              <Input type="text" placeholder="Your Name" />
+              <Input type="number" placeholder="Mobile" />
+              <Input type="number" placeholder="Alternate Mobile" />
+              <Input type="text" placeholder="Address" />
 
-    return (
-        <NativeBaseProvider>
-            <Box alignItems="center">
-                <Box w="100%" maxWidth="300px">
-                    <FormControl isRequired>
-                        <Stack mx="4">
-                            <FormControl.Label>Submit Your details</FormControl.Label>
-                            <Input type="text" placeholder="Your Name" />
-                            <Input type="number" placeholder="Mobile" />
-                            <Input type="number" placeholder="Alternate Mobile" />
-                            <Input type="text" placeholder="Address" />
+              <FormControl.ErrorMessage
+                leftIcon={<WarningOutlineIcon size="xs" />}
+              >
+                Atleast 6 characters are required.
+              </FormControl.ErrorMessage>
+              <Text>Select State</Text>
+              <Select>
+                <Select.Item label="Rajasthan" value="rj" />
+                <Select.Item label="Maharashtra" value="mh" />
+                <Select.Item label="Assam" value="as" />
+                <Select.Item label="Delhi" value="dl" />
+                <Select.Item label="Uttar Pradesh" value="up" />
+              </Select>
+              <Input type="text" placeholder="City / District" />
 
-                            <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
-                                Atleast 6 characters are required.
-                            </FormControl.ErrorMessage>
-                            <Text>Select State</Text>
-                            <Select>
-                                <Select.Item label="Rajasthan" value="rj" />
-                                <Select.Item label="Maharashtra" value="mh" />
-                                <Select.Item label="Assam" value="as" />
-                                <Select.Item label="Delhi" value="dl" />
-                                <Select.Item label="Uttar Pradesh" value="up" />
+              <Input type="number" placeholder="Pincode" />
+              <Text>Select Gender</Text>
 
-
-                            </Select>
-                            <Input type="text" placeholder="City / District" />
-                           
-                            <Input type="number" placeholder="Pincode" />
-                            <Text>Select Gender</Text>
-                            
-      <Checkbox value="one" >Male</Checkbox>
-      <Checkbox value="two">Female</Checkbox>
-      <Checkbox value="three">Other</Checkbox>
-    
-                        </Stack>
-                        <Button
-                        onPress={()=>{
-                            handleFirstForm
-                            navigation.navigate("userExperties")
-                        }}
-                        >Next</Button>
-                    </FormControl>
-                </Box>
-            </Box>;
-        </NativeBaseProvider>
-    )
+              <Checkbox value="one">Male</Checkbox>
+              <Checkbox value="two">Female</Checkbox>
+              <Checkbox value="three">Other</Checkbox>
+            </Stack>
+            <Button
+              onPress={() => {
+                handleFirstForm;
+                navigation.navigate("userExperties");
+              }}
+            >
+              Next
+            </Button>
+          </FormControl>
+        </Box>
+      </Box>
+      ;
+    </NativeBaseProvider>
+  );
 }
--- Connect_Work_Hub/componennts/bottemNavigation/tabs.js
+++ Connect_Work_Hub/componennts/bottemNavigation/tabs.js
@@ -1,24 +1,37 @@
 // import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import HomeScreen from "../screens/home/HomeScreen"
-import SearchScreen from '../screens/search/SearchScreen';
-import ProfileScreen from '../screens/profile/ProfileScreen';
+import HomeScreen from "../screens/home/HomeScreen";
+import SearchScreen from "../screens/search/SearchScreen";
+import ProfileScreen from "../screens/profile/ProfileScreen";
 // import PostScreen from '../screens/Post';
-import NotificationScreen from "../screens/notification/NotificationScreen"
+import NotificationScreen from "../screens/notification/NotificationScreen";
 
-
-
-const Tab = createBottomTabNavigator()
-
+const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
   return (
-<Tab.Navigator>
-    <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
-    <Tab.Screen name="Search" component={SearchScreen} options={{ headerShown: false }}/>
-    {/* <Tab.Screen name="Post" component={PostScreen} options={{ headerShown: false }}/> */}
-    <Tab.Screen name="Notification" component={NotificationScreen} options={{ headerShown: false }}/>
-    <Tab.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }}/>
-</Tab.Navigator>
-  )
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{ headerShown: false }}
+      />
+      {/* <Tab.Screen name="Post" component={PostScreen} options={{ headerShown: false }}/> */}
+      <Tab.Screen
+        name="Notification"
+        component={NotificationScreen}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{ headerShown: false }}
+      />
+    </Tab.Navigator>
+  );
 }
